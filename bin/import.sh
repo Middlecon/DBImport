90b32ac32eeaba497e290952d3969dad0ea467aa
Berry Österlund <berry.osterlund@middlecon.se>
2024-03-06 08:21:32 +0000

    Fixed bug in *.sh scripts

--- bin/import.sh
+++ bin/import.sh
@@ -6,3 +6,4 @@ export DBIMPORT_HOME=/usr/local/dbimport
 $DBIMPORT_HOME/bin/import $@
 if [ $? -ne 0 ]; then
 	echo "ERROR: import command was not successful"
+fi
--- bin/manage.sh
+++ bin/manage.sh
@@ -6,3 +6,4 @@ export DBIMPORT_HOME=/usr/local/dbimport
 $DBIMPORT_HOME/bin/manage $@
 if [ $? -ne 0 ]; then
 	echo "ERROR: Manage command was not successful"
+fi
