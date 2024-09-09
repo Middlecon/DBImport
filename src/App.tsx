1905a89684dc03391ac0ddab5cc719bc2f97c3ba
Anna Eklund <anna.eklund@yahoo.se>
2024-09-09 19:11:08 +0200

    Add tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import LogIn from './routes/LogIn.tsx'
 import Home from './routes/Home.tsx'
 import PrivateRoute from './components/PrivateRoute.tsx'
 import MainLayout from './components/MainLayout.tsx'
 import Import from './routes/Import.tsx'
 import Export from './routes/Export.tsx'
+
+const queryClient = new QueryClient()
 
 function App() {
   const router = createBrowserRouter([
@@ -33,7 +37,12 @@ function App() {
     }
   ])
 
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
