612dca0480ff50acef34b3c7817a30e8a562469d
Anna Eklund <anna.eklund@yahoo.se>
2024-10-24 13:31:46 +0200

    Add vite proxy .env for server
    and editor config

--- bin/Server/html/vite.config.ts
+++ bin/Server/html/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const env = loadEnv(process.env.NODE_ENV as string, process.cwd(), 'VITE_')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, '.'),
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://127.0.0.1:5188',
+        target: env.VITE_PROXY_TARGET,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
