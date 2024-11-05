f2e1401b0b06e6014eec3f94c1c78fd34de93ce0
Anna Eklund <anna.eklund@yahoo.se>
2024-11-05 21:37:05 +0100

    Add fix connection settings not initially blinking black

--- bin/Server/html/src/routes/connection/connectionDetailed/ConnectionDetailedView.tsx
+++ bin/Server/html/src/routes/connection/connectionDetailed/ConnectionDetailedView.tsx
@@ -2,20 +2,13 @@ import { useParams } from 'react-router-dom'
 import ViewBaseLayout from '../../../components/ViewBaseLayout'
 import '../../_shared/TableDetailedView.scss'
 import '../../import/tableDetailed/settings/TableSettings.scss'
-import { useConnection } from '../../../utils/queries'
-import { connectionCardRenderSettings } from '../../../utils/cardRenderFormatting'
-import CardConnection from './CardConnection'
 import '../../../components/Loading.scss'
+import ConnectionSettings from './ConnectionSettings'
 
 function ConnectionDetailedView() {
   const { connection: connectionParam } = useParams<{
     connection: string
   }>()
-  const { data: connection } = useConnection(connectionParam)
-
-  if (!connection) return <div className="loading">Loading...</div>
-
-  const connectionSettings = connectionCardRenderSettings(connection)
 
   return (
     <>
@@ -23,19 +16,7 @@ function ConnectionDetailedView() {
         <div className="table-header">
           <h1>{`${connectionParam}`}</h1>
         </div>
-        <div className="block-container" style={{ margin: 0 }}>
-          <div className="block-container-2">
-            <div>
-              <div className="cards-container">
-                <CardConnection
-                  title="Settings"
-                  settings={connectionSettings}
-                  originalData={connection}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+        <ConnectionSettings />
       </ViewBaseLayout>
     </>
   )
---
+++ bin/Server/html/src/routes/connection/connectionDetailed/ConnectionSettings.tsx
@@ -0,0 +1,36 @@
+import { useParams } from 'react-router-dom'
+import '../../_shared/TableDetailedView.scss'
+import '../../import/tableDetailed/settings/TableSettings.scss'
+import { useConnection } from '../../../utils/queries'
+import { connectionCardRenderSettings } from '../../../utils/cardRenderFormatting'
+import CardConnection from './CardConnection'
+import '../../../components/Loading.scss'
+
+function ConnectionSettings() {
+  const { connection: connectionParam } = useParams<{
+    connection: string
+  }>()
+  const { data: connection } = useConnection(connectionParam)
+
+  if (!connection) return <div className="loading">Loading...</div>
+
+  const connectionSettings = connectionCardRenderSettings(connection)
+
+  return (
+    <div className="block-container" style={{ margin: 0 }}>
+      <div className="block-container-2">
+        <div>
+          <div className="cards-container">
+            <CardConnection
+              title="Settings"
+              settings={connectionSettings}
+              originalData={connection}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ConnectionSettings
